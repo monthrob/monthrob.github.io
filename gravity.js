3ce130e7e687812d63b27dfc5f24b9a2d860414c
Monish Biswas <monish.biswas@openbet.com>
2015-02-16 00:19:15 +0000

    Refactor
    Move physics stuff into gravity.js

--- gravity.js
+++ gravity.js
@@ -14,21 +14,64 @@ var Gravity = {};
 // Creates a universe
 Gravity.Universe = function () {
 //  List of bodies
-	var bodies = [];
+	this.bodies = {};
+
+	//  G - Gravitational constant
+	//  Should be  6.673e-11;
+	//  but pick a value by trial end error
+	//  that animates well
+	this.gravity_constant = 1e-5;
+	//  This is in mass units, earth mass's
+	this.centre_mass = 10000
+	this.max_r = 2;
+	this.elastic_factor  = 1;
+	this.max_speed   = 0.1;
+	this.slowdown_factor = 1;
+}
+
+Gravity.Universe.prototype = {
+	addBody: function (id,  mass, pos, vel) {
+		this.bodies[id] = new Gravity.Body(mass, pos, vel,this);
+	},
+	iterate: function (time) {
+		//  Force relative to position from origin
+		for (var i in this.bodies) {
+			var body = this.bodies[i];
+			//  Gravitational pull
+			// Kind of m1*m1*G / r^2
+			var body_mass = body.mass
+			var factor = body_mass * this.centre_mass * this.gravity_constant;
+			var r = body.position.modulus();
+			var force = body.position.scale(-1).direction().scale(factor/(r*r));
+			body.addForce(force);
+			//  Also add a elastic force to bring it back
+			if (r > this.max_r) {
+				var elastic_force = body.position.scale(-1).scale(this.elastic_factor);
+				body.addForce(elastic_force);
+			}
+			// Also retadr it if is it too fast 
+			var speed = body.velocity.modulus()
+			if (speed > this.max) {
+				var slowdown_force = body.velocity.scale(-1).scale(this.slowdown_factor);
+				body.addForce(slowdown_force);
+			}
+			body.newFrame(time);
+		}
+	}
+	
 	
 }
 
 
 
-//  G - Gravitational constant
-Gravity.Universe.G =  6.673e-11;
 
 //  Body class 
 
-Gravity.Body = function(mass,position,velocity) {
+Gravity.Body = function(mass,position,velocity,universe) {
 	this.mass     = mass;
 	this.position = position;
 	this.velocity = velocity;
+	this.universe = universe;
 	this.forces = [];
 }
 
@@ -61,12 +104,20 @@ Gravity.Body.prototype = {
 		for (var i in this.forces) {
 			var force = this.forces[i];			
 			//  Add in the acceleration
-			accel = accel.add(force.scale(1/this.mass));
+			accel = accel.add(force.scale(1/(this.mass)));
 		}
 		this.forces = [];
+		// Equation of motion = 
+		// r = ro + vt + 0.5*a*t^2
+		// New Position - from Velocity 
+		this.position = this.position.add(this.velocity.scale(time));
+		// Now consider acceleration 0.5 * a * t^2
+//		this.position = this.position.add(accel.scale(0.5).scale(time*time))
+		// Note the correct equation has a 0.5 in there, but a better animation 
+		// is produced with an incorrect equation.
+		this.position = this.position.add(accel.scale(1).scale(time*time))
 		//  New Velocity
 		this.velocity = this.velocity.add(accel.scale(time));
-		this.position = this.position.add(this.velocity.scale(time));
 	}
 }
 
