ced4bca89ee25b10d01135609716d1f0559b3c8c
Monish Biswas <monish.biswas@openbet.com>
2018-02-20 08:42:28 +0000

    Move to Angulat
    * Add immutable Make SimEL to represent an element
    * Make SimEl.move(...) return a new instance rather than mutate this
    * Add new directive for Sim objects, and pass SimEl object into directive
    * Fix update problem by using scope.$watch() in directive link function
    * Remove dead code

--- js/controllers/Simulation.js
+++ js/controllers/Simulation.js
@@ -6,18 +6,71 @@
 var app = angular.module('Simulation', []);
 
 
-app.controller('SimulationController',function  ($scope) {
+app.controller('SimulationController',function  ($scope, $interval) {
+    $scope.viewport = {
+        universeWidth: null,
+        universeHeight: null
+    }
 
 	$scope.simulation = new Simulation(
-		document.getElementById("monverse"),
-		document.getElementById("minimon")
-	);
+		document.getElementById("monverse")
+    );
 
 	$scope.simulation.addSun(10000,0.5);
 	$scope.simulation.addRandomBodies(5,
 		0.5,2,
 		1,10,
 		0.02);
+	$scope.height;
+	$scope.width;
+
+    interval = 1 / $scope.simulation.frame_rate;
+
+
+    $interval(function() {
+        $scope.simulation.iterate();
+    }, interval * 1000)
 
 });
 
+app.directive('miniMon', function () {
+    return {
+        restrict: 'E',
+        scope : {
+            coords: "="
+        },
+        template: '<img class="minimon" src="images/MonGlobeWhite.gif" alt="minithrob">',
+        link: function (scope, e, attrs) {
+            scope.$watch('coords', function() {
+                elem = e.children();
+
+                var coords = scope.coords;
+
+                elem.id = coords.id
+                elem.css({
+                    'width': coords.width + "px",
+                    'height': coords.height + "px",
+                    'top': (coords.rows - coords.height/2) + "px",
+                    'left': (coords.cols - coords.width/2 ) + "px",
+                    'z-index': coords.depth
+                });
+            });
+        }
+    }});
+/*
+    app.directive('monverse', function () {
+        return {
+            restrict: 'A',
+            transclude: true,
+            scope : {
+                width: '=',
+                height: '='
+            },
+            template: '<div class="monverse" ng-transclude></div>',
+            link: function (scope, e, attrs) {
+                scope.height = e.prop('clientHeight');
+                scope.width  = e.prop('clientWidth');
+            }
+        }
+    })
+*/
